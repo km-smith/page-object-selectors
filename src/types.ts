622d11b581ddd726ab31273b2508e87015b2bb7d
Kyle Smith <kyle.mark.smith@outlook.com>
2020-12-19 03:54:18 +0000

    Cleanup whitespace in types.ts file

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,8 @@
 type Query = 'Query';
 type QueryAll = 'QueryAll';
 type Mode = Query | QueryAll;
-
 type Children = Record<string, Schema>;
 type Scope = [Mode, string];
-
 type QueryResult = undefined | Element | Element[];
 
 interface Schema {
